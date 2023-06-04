e7ea1dd81429ceb867564a13925b56b6bca9f2cd
Chamath Wijerathne <anuradhika.wije@gmail.com>
2023-06-04 18:07:26 +0300

    Added auth modal and implement login and register pages. Install react-icons.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import Link from 'next/link'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Head>
+      <title>LeetCode clone</title>
+      <meta name='viewport' content='width-device-width, initial-scale=1' />
+      <Link rel='icon' href='/favicon.png' />
+      <meta name='description' content='Web application containd problems and solutions' />
+    </Head>
+     <Component {...pageProps} />
+    </>
+  )
 }
